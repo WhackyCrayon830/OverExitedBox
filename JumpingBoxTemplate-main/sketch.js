6135c7ff74f9d5f58adbda11720eb2e2466fb4e6
WhackyCrayon830 <75725021+WhackyCrayon830@users.noreply.github.com>
2021-11-01 20:37:30 +0530

    Add files via upload

---
+++ JumpingBoxTemplate-main/sketch.js
@@ -0,0 +1,55 @@
+var canvas;
+var music;
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    //create 4 different surfaces
+block1 = createSprite(0, 580, 360, 30);
+block1.shapeColor = "#c9eb34";
+block2 = createSprite(295, 580, 200, 30);
+block2.shapeColor = "#34eb93";
+block3 = createSprite(515, 580, 200, 30);
+block3.shapeColor = "#ab34eb";
+block4 = createSprite(740, 580, 220, 30);
+block4.shapeColor = "#eb3486";
+
+
+    //create box sprite and give velocity
+ball = createSprite(random(20, 750), 100, 40, 40);
+ball.shapeColor = "#000000";
+ball.velocityX = 4;
+ball.velocityY = 9;
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    //create edgeSprite
+edges = createEdgeSprites();
+ball.bounceOff(edges);
+if(block1.isTouching(ball) && ball.bounceOff(block1)){
+    ball.shapeColor = ("#c9eb34")
+    music.play();
+}
+if(block2.isTouching(ball)){
+    ball.shapeColor = ("#34eb93")
+    ball.velocityX = 0;
+    ball.velocityY = 0
+    music.stop();
+}
+if(block3.isTouching(ball) && ball.bounceOff(block3)){
+    ball.shapeColor = ("#ab34eb")
+    music.play();
+}
+if(block3.isTouching(ball) && ball.bounceOff(block4)){
+    ball.shapeColor = ("#eb3486")
+    music.play();
+}
+    //add condition to check if box touching surface and make it box
+    drawSprites();
+}
